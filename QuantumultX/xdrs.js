7a5a5605ffd3b7962df7f8708c23955650dbba42
Xiao Li <xiaoli3397@gmail.com>
2023-11-15 14:48:54 +0800

    Update xdrs.js

--- QuantumultX/xdrs.js
+++ QuantumultX/xdrs.js
@@ -3,19 +3,21 @@ const cookieKey = 'xiao_cookie_xdrs'
 const cookieVal = $prefs.valueForKey(cookieKey)
 
 function sign() {
-  let request = {
+  let url = {
     url: `https://rs.xidian.edu.cn/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1&inajax=1`,
     method: `POST`,
     headers: {
       Cookie: cookieVal
     }
   }
-  $task.fetch(request).then(response => {
-    let data = response.body
-    
-    console.log(response.statusCode + "\n\n" + response.body);
-    $done();
-  }, reason => {
-    console.log(reason.error);
-    $done();
-  });
+  $task.fetch(url).then(response => {
+    if(true) {
+      let title = `${cookieName}`
+      let subTitle = `签到结果: 签到跳过`
+      let detail = `今天已经签过了`
+      console.log(`${title}, ${subTitle}, ${detail}`)
+      $notify(title, subTitle, detail)
+      $done()
+    }
+  }
+});
